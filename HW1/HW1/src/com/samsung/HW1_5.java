9b18279bf2e52a372de65ecdbc6135e5f07238cf
KKakavo <pooreshqa2805@gmail.com>
2022-10-06 17:50:16 +0300

    1

---
+++ HW1/HW1/src/com/samsung/HW1_5.java
@@ -0,0 +1,15 @@
+package com.samsung;
+
+import java.util.Scanner;
+
+public class HW1_5
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        double x = in.nextDouble();
+        double y = in.nextDouble();
+        String ans = ((x<0 && x*x+y*y<1) || (y>x && x*x+y*y<1))? "YES" : "NO";
+        System.out.println(ans);
+    }
+}
