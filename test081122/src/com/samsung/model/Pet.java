d71e53bd82b0c07e2b7927703443d65a31d33d76
KKakavo <pooreshqa2805@gmail.com>
2022-11-08 20:32:17 +0300

    4 задание

--- test081122/src/com/samsung/model/Pet.java
+++ test081122/src/com/samsung/model/Pet.java
@@ -7,14 +7,23 @@ public class Pet {
     private String ownerName;
     private String petType;
     private String[] visits;
-
+    private int index;
     public Pet(String petName, String ownerName, String petType) {
         this.petName = petName;
         this.ownerName = ownerName;
         this.petType = petType;
         String[] visits = new String[10];
     }
-
+    public void addVisit(String visit){
+        visits[index++]=visit;
+        if(index == visits.length()){
+            String[] temp = new String[visits.length];
+            String visits = new String[visits.length+10];
+            for (int i = 0; i < temp.length; i++) {
+                visits[i] = temp[i];
+            }
+        }
+    }
     @Override
     public String toString() {
         StringBuilder stringBuilder = new StringBuilder();
