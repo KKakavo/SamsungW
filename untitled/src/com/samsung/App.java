fb027a133afec8bb66a586110d7bfb69cc2ea59e
KKakavo <pooreshqa2805@gmail.com>
2023-01-28 00:48:59 +0300

    A

---
+++ untitled/src/com/samsung/App.java
@@ -0,0 +1,11 @@
+package com.samsung;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        Integer.parseInt()
+    }
+}
