fc13a3500897db17459036f36aa774411d7db0a0
KKakavo <pooreshqa2805@gmail.com>
2023-02-21 15:40:06 +0300

    l

---
+++ mc-spring-security/src/main/java/com/example/mcspringsecurity/McSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.mcspringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McSpringSecurityApplication.class, args);
+	}
+
+}
---
+++ mc-spring-security/src/test/java/com/example/mcspringsecurity/McSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mcspringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
